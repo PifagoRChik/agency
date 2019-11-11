1221b7a187a5158f79a9deec76cef73c8765b5b0
PifagoRChik <52865593+PifagoRChik@users.noreply.github.com>
2019-11-11 16:49:20 +0300

    create project

---
+++ agency/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace agency
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.enterButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.topPanel = new System.Windows.Forms.Panel();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.minimizeButton = new System.Windows.Forms.Button();
+            this.loginInput = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            this.passwordInput = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            this.topPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // enterButton
+            // 
+            this.enterButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(95)))), ((int)(((byte)(92)))));
+            this.enterButton.FlatAppearance.BorderSize = 0;
+            this.enterButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.enterButton.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.enterButton.ForeColor = System.Drawing.Color.White;
+            this.enterButton.Location = new System.Drawing.Point(450, 370);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(93, 29);
+            this.enterButton.TabIndex = 9;
+            this.enterButton.Text = "Войти";
+            this.enterButton.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Silver;
+            this.label1.Location = new System.Drawing.Point(347, 214);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(299, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Для входа в систему введите логин и пароль";
+            // 
+            // topPanel
+            // 
+            this.topPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(95)))), ((int)(((byte)(92)))));
+            this.topPanel.Controls.Add(this.exitButton);
+            this.topPanel.Controls.Add(this.minimizeButton);
+            this.topPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topPanel.Location = new System.Drawing.Point(0, 0);
+            this.topPanel.Name = "topPanel";
+            this.topPanel.Size = new System.Drawing.Size(1004, 35);
+            this.topPanel.TabIndex = 7;
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("exitButton.BackgroundImage")));
+            this.exitButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.exitButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.exitButton.FlatAppearance.BorderSize = 0;
+            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitButton.Location = new System.Drawing.Point(962, 3);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(33, 29);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // minimizeButton
+            // 
+            this.minimizeButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("minimizeButton.BackgroundImage")));
+            this.minimizeButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.minimizeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.minimizeButton.FlatAppearance.BorderSize = 0;
+            this.minimizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minimizeButton.Location = new System.Drawing.Point(923, 3);
+            this.minimizeButton.Name = "minimizeButton";
+            this.minimizeButton.Size = new System.Drawing.Size(33, 29);
+            this.minimizeButton.TabIndex = 5;
+            this.minimizeButton.UseVisualStyleBackColor = true;
+            this.minimizeButton.Click += new System.EventHandler(this.minimizeButton_Click);
+            // 
+            // loginInput
+            // 
+            this.loginInput.BorderColorFocused = System.Drawing.Color.Blue;
+            this.loginInput.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.loginInput.BorderColorMouseHover = System.Drawing.Color.Blue;
+            this.loginInput.BorderThickness = 3;
+            this.loginInput.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.loginInput.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.loginInput.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.loginInput.isPassword = false;
+            this.loginInput.Location = new System.Drawing.Point(311, 248);
+            this.loginInput.Margin = new System.Windows.Forms.Padding(4);
+            this.loginInput.Name = "loginInput";
+            this.loginInput.Size = new System.Drawing.Size(370, 44);
+            this.loginInput.TabIndex = 10;
+            this.loginInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // passwordInput
+            // 
+            this.passwordInput.BorderColorFocused = System.Drawing.Color.Blue;
+            this.passwordInput.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.passwordInput.BorderColorMouseHover = System.Drawing.Color.Blue;
+            this.passwordInput.BorderThickness = 3;
+            this.passwordInput.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.passwordInput.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.passwordInput.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.passwordInput.isPassword = false;
+            this.passwordInput.Location = new System.Drawing.Point(311, 300);
+            this.passwordInput.Margin = new System.Windows.Forms.Padding(4);
+            this.passwordInput.Name = "passwordInput";
+            this.passwordInput.Size = new System.Drawing.Size(370, 44);
+            this.passwordInput.TabIndex = 10;
+            this.passwordInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 601);
+            this.Controls.Add(this.passwordInput);
+            this.Controls.Add(this.loginInput);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.topPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.topPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button enterButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel topPanel;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button minimizeButton;
+        private Bunifu.Framework.UI.BunifuMetroTextbox loginInput;
+        private Bunifu.Framework.UI.BunifuMetroTextbox passwordInput;
+    }
+}
+
